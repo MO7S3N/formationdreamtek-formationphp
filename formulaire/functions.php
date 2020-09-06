45d893dbf1ec1eb213691bb1b219c690a0a2283f
MO7S3N <34940993+MO7S3N@users.noreply.github.com>
2020-09-07 00:21:59 +0100

    formulaire v4

--- formulaire/functions.php
+++ formulaire/functions.php
@@ -1,11 +1,28 @@
 <?php
 function checkbox(string $name, string $value, array $data)
 {  $attributes='';
-   $attributes .='checked'; 
+   if (isset($data[$name])&& in_array($value, $data[$name]))
+   {
+    $attributes .='checked'; 
+   }
+     
 ?>
-
-   <input type="checkbox" name="{name}[]" value="<?php echo $value; ?>" $attributes>
+   <input type="checkbox" name="{$name}[]" value="<?php echo $value; ?>" $attributes>
 <?php
 }
 ?>
+
+<?php
+ function radio(string $name, string $value, array $data)
+ {$attributes='';
+    if (isset($data[$name])&& ($value==$data[$name]))
+    {
+     $attributes .='checked'; 
+    }
+      
+ ?>
+    <input type="radio" name="<?php echo $name ?>" value="<?php echo $value; ?>" $attributes>
+ <?php
+ }
+ ?>
  
--- formulaire/jeu.php
+++ formulaire/jeu.php
@@ -35,10 +35,50 @@ $cornets=[
     'cornet'=>3
 ];
 //checkbox
-$supplement=[
+$supplements=[
     'pepite de chocolat'=>1,
     'chantille'=>0.5
 ];
+
+$ingredients=[];
+$total=0;
+if (isset($_GET['parfum']))
+{
+    foreach($_GET['parfum'] as $parfum)
+     {
+         if(isset($parfums[$parfum]))
+          {
+              $ingredients[]=$parfum;
+              $total += $parfums[$parfum];
+          }
+     }
+
+}
+
+if (isset($_GET['supplement']))
+{
+    foreach($_GET['supplement'] as $supplement)
+     {
+         if(isset($supplements[$supplement]))
+          {   
+              $ingredients[]=$supplement;
+              $total += $supplements[$supplement];
+          }
+     }
+
+}
+
+if (isset($_GET['cornet']))
+{   
+    $cornet=$_GET['cornet'];
+     {
+         if(isset($cornets[$cornet]))
+          {
+              $ingredients[]=$cornet;
+              $total += $cornets[$cornet];
+          }
+     }
+}
 
 require 'header.php';
 ?>
@@ -64,17 +104,56 @@ elseif($succes):
 
 
 <h1>composer votre glace</h1>
-<form action="jeu.php" method="GET">
-    <?php foreach($parfums as $parfum => $prix): ?>
-    <div class="checkbox">
-        <label>
-        <?php checkbox ('parfum', $parfum , $_GET); ?>
-        <?php  echo $parfum; ?> - <?php echo $prix; ?> £
-        </label>
-    </div>    
-    <?php endforeach; ?>   
-    <button type="submit">composer ma classe </button>
-</form>
+<div class="row">
+   <div class="col-md-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">votre glace</h5>
+                    <ul>
+                    <?php foreach($ingredients as $ingredient):?>
+                    <li><?php echo $ingredient; ?></li>
+                    <li><?php echo $total; ?></li>
+                    <?php
+                    endforeach ;
+                    ?>
+                    </ul>
+            </div>
+        </div>
+   </div> 
+   <div class="col-md-8">
+        <form action="jeu.php" method="GET">
+            <h2>choisissez votre parfum</h2>
+            <?php foreach($parfums as $parfum => $prix): ?>
+            <div class="checkbox">
+                <label>
+                <?php checkbox ('parfum', $parfum , $_GET); ?>
+                <?php  echo $parfum; ?> - <?php echo $prix; ?> £
+                </label>
+            </div>    
+            <?php endforeach; ?>   
+            <h2>choisissez votre cornet</h2>
+            <?php foreach($cornets as $cornet =>$prix): ?>
+            <div class="radio">
+                <label>
+                <?php radio ('cornet', $cornet , $_GET); ?>
+                <?php  echo $cornet; ?> - <?php echo $prix; ?> £
+                </label>
+            </div>    
+            <?php endforeach; ?>   
+            <h2>choisissez votre supplement</h2>
+            <?php foreach($supplements as $supplement =>$prix): ?>
+            <div class="ckeckbox">
+                <label>
+                <?php checkbox ('supplement', $supplement , $_GET); ?>
+                <?php  echo $supplement; ?> - <?php echo $prix; ?> £
+                </label>
+            </div>    
+            <?php endforeach; ?>   
+
+            <button type="submit" class="btn btn-primary">composer ma classe </button>
+        </form>
+   </div>
+</div>
 
 <pre>
 <?php var_dump($_GET);  ?>
