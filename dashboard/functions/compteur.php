96ad3a0a227ea24de8a106f52bad9414c04262f0
MO7S3N <34940993+MO7S3N@users.noreply.github.com>
2020-09-10 15:12:36 +0100

    dashboard v3

--- dashboard/functions/compteur.php
+++ dashboard/functions/compteur.php
@@ -17,7 +17,7 @@ function increment_compteur(string $fichier)
         $compteur++;
        
     }
-      file_put_contents($fichier,$compteur); 
+      file_put_contents($fichier,$compteur);
 
    
 }
@@ -47,19 +47,20 @@ function nombres_devues_detail_mois(int $annee , int $mois)
     $mois=str_pad($mois,2,'0', STR_PAD_LEFT );
     $fichier = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'compteur-' . $annee . '-' . $mois . '-' . '*' ;
     $fichiers=glob($fichier);
-    $visites=[];
+    $visites = [];
+
     foreach($fichiers as $fichier)
     {
-       $parties =explode('-',basename($fichier));
-       $detail[]=
+       $parties = explode('-',basename($fichier));
+       $visites[]=
        [
            'annee'=>$parties[1],
            'mois'=>$parties[2],
            'jour'=>$parties[3],
-           'visites'=>file_get_contents($fichier)
-
+           'visites'=> file_get_contents($fichier)
        ];
-       
+    
+    return $visites;   
     }
     
 }
