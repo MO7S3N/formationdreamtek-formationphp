e25abbb02465b26ef8f98b82b7d08f03b286ac5b
MO7S3N <34940993+MO7S3N@users.noreply.github.com>
2020-09-10 14:53:10 +0100

    dashboard v2

--- dashboard/functions/compteur.php
+++ dashboard/functions/compteur.php
@@ -28,5 +28,40 @@ function nombres_devues()
     return file_get_contents($fichier);
 }
 
+function nombres_devues_mois(int $annee , int $mois)
+{
+    $mois=str_pad($mois,2,'0', STR_PAD_LEFT );
+    $fichier = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'compteur-' . $annee . '-' . $mois . '-' . '*' ;
+    $fichiers=glob($fichier);
+    $total=0;
+    foreach($fichiers as $fichier)
+    {
+        $total += (int) file_get_contents($fichier);
+    }
+    return $total;
+    
+}
+
+function nombres_devues_detail_mois(int $annee , int $mois)
+{
+    $mois=str_pad($mois,2,'0', STR_PAD_LEFT );
+    $fichier = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'compteur-' . $annee . '-' . $mois . '-' . '*' ;
+    $fichiers=glob($fichier);
+    $visites=[];
+    foreach($fichiers as $fichier)
+    {
+       $parties =explode('-',basename($fichier));
+       $detail[]=
+       [
+           'annee'=>$parties[1],
+           'mois'=>$parties[2],
+           'jour'=>$parties[3],
+           'visites'=>file_get_contents($fichier)
+
+       ];
+       
+    }
+    
+}
 ?>
 
