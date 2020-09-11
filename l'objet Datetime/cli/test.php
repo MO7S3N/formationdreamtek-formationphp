70bbf723cef59b2553ed69ba1e01ab1bfc051ea9
MO7S3N <34940993+MO7S3N@users.noreply.github.com>
2020-09-11 15:36:30 +0100

    les classes

--- l'objet Datetime/cli/test.php
+++ l'objet Datetime/cli/test.php
@@ -3,4 +3,4 @@ $date=new DateTime('2019-01-01');
 $interval = new DateInterval('P1M1DT1M');
 $date->add($interval);
 var_dump($interval);
-?>
+?> 
---
+++ lesclasses/menu_functions.php
@@ -0,0 +1,24 @@
+<?php
+function nav_item(string $lien, string $titre, string $linkClass='')
+{
+     $classe= 'nav-item';
+     if($_SERVER['SCRIPT_NAME']== $lien)
+     {
+         $classe .= 'active';
+     }
+
+?>
+
+<li class="<?php $classe; ?>">
+<a class="nav-link" href="<?php $lien; ?>"><?php echo $titre; ?></a>
+</li>
+<?php
+}
+?>
+
+<?php
+function nav_menu(string $linkClass='')
+{
+return nav_item('/index.php', 'Acceuil', $linkClass) . nav_item('/contact.php', 'Contact', $linkClass) . nav_item('/menu.php', 'Menu', $linkClass);
+}
+?>
